d4afb06ac8ea0c8a3f781fd86ba83c9504b8a524
Andy Longworth <alongworthis@gmail.com>
2012-11-13 11:05:29 +1100

    Initial Commit
    Lab 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shouter::Application.config.secret_token = 'abaad5c1efff3652c57110e6a586aef85228ecd9538f59e0936101af61352cadeefe7e744f8ffd04de3c588354bd93e665fe10d9bce5384bea5da9b37a837615'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Shouter::Application.routes.draw do
+  match ':controller(/:action(/:id))(.:format)'
+end
